02006e55b6f76fd60bb12d12986a7d2ac48de1bd
yanapang <yanahwang@gmail.com>
2023-03-02 22:11:59 +0900

    [인프런 스프링 입문] h2 DB 연결.

---
+++ src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package hello.hellospring.controller;
+
+public class MemberForm {
+    private String name;//form의 name값이 매칭되어 들어옴.
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
